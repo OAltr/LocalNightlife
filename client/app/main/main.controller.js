3534d6fb046695b83cfbd3542a9e6ea1cff9f747
Oscar <oscar.altrogge@me.com>
2015-07-15 22:58:40 +0200

    Added basic structure for locations

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,6 +2,34 @@
 
 angular.module('localNightlifeApp')
 	.controller('MainCtrl', function ($scope, $http, socket) {
+		$scope.searchString = '';
+		$scope.locations = [];
+
+		$scope.startSearching = function (searchString) {
+			if(searchString === '')
+				return;
+
+			$scope.locations = [{
+				name: 'Bar One',
+				going: 3,
+				userGoing: false
+			}, {
+				name: 'Bar Two',
+				going: 4,
+				userGoing: true
+			}, {
+				name: 'Bar Three',
+				going: 5,
+				userGoing: false
+			}];
+		};
+
+		$scope.addMe = function(location) {
+			if(!location.userGoing) {
+				location.userGoing = true;
+				location.going++;
+			}
+		};
 
 		$scope.$on('$destroy', function () {
 		});
