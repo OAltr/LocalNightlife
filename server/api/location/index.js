2350b0091138df20cf6cc01fb9f76b50609e6cb1
Oscar <oscar.altrogge@me.com>
2015-07-17 01:04:17 +0200

    Setup the location API to respond with yelp results

--- server/api/location/index.js
+++ server/api/location/index.js
@@ -5,11 +5,8 @@ var controller = require('./location.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/:city/:userID?', controller.show);
+router.post('/:locationID/:userID', controller.create);
+router.delete('/:locationID/:userID', controller.destroy);
 
 module.exports = router;
--- server/api/location/location.controller.js
+++ server/api/location/location.controller.js
@@ -2,58 +2,86 @@
 
 var _ = require('lodash');
 var Location = require('./location.model');
+var config = require('../../config/environment');
+var yelp = require('yelp').createClient({
+	consumer_key: config.yelp.consumer_key,
+	consumer_secret: config.yelp.consumer_secret,
+	token: config.yelp.token,
+	token_secret: config.yelp.token_secret
+});
 
-// Get list of locations
-exports.index = function(req, res) {
-  Location.find(function (err, locations) {
-    if(err) { return handleError(res, err); }
-    return res.json(200, locations);
-  });
-};
+var today = function() {
+	var dateObj = new Date();
+	var month = dateObj.getUTCMonth() + 1; //months from 1-12
+	var day = dateObj.getUTCDate();
+	var year = dateObj.getUTCFullYear();
 
-// Get a single location
+	return day + "/" + month + "/" + year;
+}
+
+// Get near locations
 exports.show = function(req, res) {
-  Location.findById(req.params.id, function (err, location) {
-    if(err) { return handleError(res, err); }
-    if(!location) { return res.send(404); }
-    return res.json(location);
-  });
+	var city = req.params.city || '';
+	var user = req.params.userID || '';
+
+	yelp.search({term: "bar", location: city}, function(error, data) {
+		var locationIDs = data.businesses.map(function(business) {
+			return business.id;
+		});
+
+		Location.find({location: {$in: locationIDs}, date: today()}, function (err, allLocations) {
+			if(err) { return handleError(res, err); }
+			if(!allLocations) { return res.send(404); }
+
+			var locations = data.businesses.map(function(business) {
+				return {
+					id: business.id,
+					name: business.name,
+					url: business.url,
+					going: allLocations.filter(function(location) {
+						return location.location === business.id;
+					}).length,
+					userGoing: (allLocations.filter(function(location) {
+						return location.location === business.id && location.user === user;
+					}).length > 0)
+				};
+			});
+
+			return res.json(locations);
+		})
+	});
 };
 
 // Creates a new location in the DB.
 exports.create = function(req, res) {
-  Location.create(req.body, function(err, location) {
-    if(err) { return handleError(res, err); }
-    return res.json(201, location);
-  });
-};
+	var location = req.params.locationID;
+	var user = req.params.userID;
 
-// Updates an existing location in the DB.
-exports.update = function(req, res) {
-  if(req.body._id) { delete req.body._id; }
-  Location.findById(req.params.id, function (err, location) {
-    if (err) { return handleError(res, err); }
-    if(!location) { return res.send(404); }
-    var updated = _.merge(location, req.body);
-    updated.save(function (err) {
-      if (err) { return handleError(res, err); }
-      return res.json(200, location);
-    });
-  });
+	var newLocation = new Location();
+	newLocation.user = user;
+	newLocation.location = location;
+	newLocation.date = today();
+
+	newLocation.save(function(err, location) {
+		if(err) { return handleError(res, err); }
+		return res.json(201, location);
+	});
 };
 
 // Deletes a location from the DB.
 exports.destroy = function(req, res) {
-  Location.findById(req.params.id, function (err, location) {
-    if(err) { return handleError(res, err); }
-    if(!location) { return res.send(404); }
-    location.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
-  });
+	var location = req.params.locationID;
+	var user = req.params.userID;
+	Location.find({location: location, user: user, date: today()}, function (err, location) {
+		if(err) { return handleError(res, err); }
+		if(!location) { return res.send(404); }
+		location.remove(function(err) {
+			if(err) { return handleError(res, err); }
+			return res.send(204);
+		});
+	});
 };
 
 function handleError(res, err) {
-  return res.send(500, err);
+	return res.send(500, err);
 }
